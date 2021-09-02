d3da8db693b4a1893e688bb2a9efeda13f75b56a
khairold <khairold@gmail.com>
2021-09-02 16:58:03 +0800

    marker

--- components/SmartMap.js
+++ components/SmartMap.js
@@ -1,19 +1,44 @@
+import { useState, useMemo, useRef, useCallback } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
 import 'leaflet-defaulticon-compatibility'
 
+const center = {
+  lat: 3.1399616034165456,
+  lng: 101.77296128020288,
+}
+
 const SmartMap = () => {
+  const [position, setPosition] = useState(center)
+  const markerRef = useRef(null)
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current
+        if (marker != null) {
+          setPosition(marker.getLatLng())
+        }
+      },
+    }),
+    []
+  )
   return (
-    <MapContainer center={[40.8054, -74.0241]} zoom={14} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
-      />
-      <Marker position={[40.8054, -74.0241]} draggable={true} animate={true}>
-        <Popup>Hey ! I live here</Popup>
-      </Marker>
-    </MapContainer>
+    <div style={{ height: 800 }}>
+      {JSON.stringify(position)}
+      <MapContainer center={center} zoom={14} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
+        />
+
+        <Marker draggable={true} eventHandlers={eventHandlers} position={position} ref={markerRef}>
+          <Popup minWidth={90}>
+            <span>This is the Marker</span>
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   )
 }
 
--- pages/map.js
+++ pages/map.js
@@ -1,9 +1,10 @@
 import dynamic from 'next/dynamic'
+// import SmartMap from '../components/SmartMap'
 const SmartMap = dynamic(() => import('../components/SmartMap'), { ssr: false })
 
 function Map() {
   return (
-    <div id="map">
+    <div style={{ height: 800 }}>
       <SmartMap />
     </div>
   )
